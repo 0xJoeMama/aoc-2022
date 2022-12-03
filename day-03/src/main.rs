58021fb9a39dd33a9b3b6a82b90d9363969cbf27
0xJoeMama <0xjoemama@gmail.com>
2022-12-03 13:58:09 +0200

    minor improvements + timing

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -4,25 +4,29 @@ use aoc_lib::input;
 
 fn calculate_score<'a, T>(dups: T) -> i32
 where
-    T: Iterator<Item = &'a char>,
+    T: IntoIterator<Item = char>,
 {
-    let mut sum: i32 = 0;
-    for c in dups {
+    dups.into_iter().fold(0, |acc, c| {
         if c.is_lowercase() {
-            sum += ((*c as u8) - 96) as i32;
+            acc + ((c as u8) - 96) as i32
         } else {
-            sum += ((*c as u8) - 65 + 27) as i32;
+            acc + ((c as u8) - 65 + 27) as i32
         }
-    }
-
-    sum
+    })
 }
 
 fn main() {
     let _ = input::apply("input-day-03.txt", |file| {
-        let res = file.split("\n").filter(|l| !l.is_empty()).collect();
-        println!("{}", part1(&res));
-        println!("{}", part2(&res));
+        aoc_lib::timed(|| {
+            let res = file.split("\n").filter(|l| !l.is_empty()).collect();
+            aoc_lib::timed(|| {
+                println!("{}", part1(&res));
+            });
+
+            aoc_lib::timed(|| {
+                println!("{}", part2(&res));
+            });
+        });
     });
 }
 
@@ -31,31 +35,25 @@ fn part1(input: &Vec<&str>) -> i32 {
         .iter()
         .map(|sack| {
             let (first, second) = sack.split_at(sack.len() / 2);
-            let unique = first.chars().collect::<HashSet<_>>();
-            second
-                .chars()
-                .filter(|c| unique.contains(&c))
-                .collect::<HashSet<_>>()
+            let intersect = first.chars().filter(|c| second.contains(*c));
+            calculate_score(intersect.collect::<HashSet<_>>())
         })
-        .map(|dups| calculate_score(dups.iter()))
         .sum()
 }
 
 fn part2(input: &Vec<&str>) -> i32 {
-    let mut badges: Vec<char> = Vec::new();
+    let mut badges = Vec::with_capacity(input.len() / 3);
 
     for i in 0..input.len() / 3 {
         let values = &input[i * 3..(i + 1) * 3];
-
         let badge = values[0]
             .chars()
-            .filter(|c| values[1].contains(*c))
-            .filter(|c| values[2].contains(*c))
+            .filter(|c| values[1].contains(*c) && values[2].contains(*c))
             .last()
             .unwrap();
 
         badges.push(badge);
     }
 
-    calculate_score(badges.iter())
+    calculate_score(badges)
 }
