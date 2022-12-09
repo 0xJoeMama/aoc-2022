570eca46c2b75c3e031358a11e82130542addcc8
0xJoeMama <0xjoemama@gmail.com>
2022-12-09 19:00:29 +0200

    day 09

--- aoc-lib/src/point.rs
+++ aoc-lib/src/point.rs
@@ -1,18 +1,30 @@
 use std::{
     fmt::Display,
-    ops::{Add, Mul, Neg, AddAssign},
+    ops::{Add, Mul, Neg, AddAssign, Sub, Div},
 };
 
 #[derive(Debug, Eq, Hash, PartialEq, Clone, Copy, PartialOrd)]
 pub struct Point {
-    x: i64,
-    y: i64,
-    z: i64,
+    pub x: i64,
+    pub y: i64,
+    pub z: i64,
 }
 
 impl Display for Point {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("({}, {}, {})", self.x, self.y, self.z))
+    }
+}
+
+impl Div<i64> for Point {
+    type Output = Point;
+
+    fn div(self, rhs: i64) -> Self::Output {
+        Point {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
     }
 }
 
@@ -68,6 +80,31 @@ impl AddAssign<Point> for Point {
     }
 }
 
+impl Sub<Point> for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Point) -> Self::Output {
+        Point {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl Sub<&Point> for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: &Point) -> Self::Output {
+        Point {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+
+}
+
 impl AddAssign<&Point> for Point {
     fn add_assign(&mut self, rhs: &Point) {
         self.x += rhs.x;
@@ -101,8 +138,13 @@ impl Point {
     pub fn new(x: i64, y: i64, z: i64) -> Self {
         Self { x, y, z }
     }
+
+    pub fn distance_squared(&self, other: &Point) -> i64 {
+        (self.x - other.x).pow(2) + (self.y - other.y).pow(2) + (self.z - other.z).pow(2)
+    }
 }
 
+#[derive(Debug, Eq, Hash, PartialEq, Clone, Copy, PartialOrd)]
 pub enum Direction {
     PosX,
     NegX,
@@ -133,5 +175,17 @@ impl Direction {
 
     pub fn plane() -> &'static [Direction; 4] {
         &Self::PLANE
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn distance() {
+        let a = Point::new(3, 0, 0);
+        let b = Point::new(4, 0, 0);
+        assert!(a.distance_squared(&b) == 1);
+
     }
 }
---
+++ day-09/src/main.rs
@@ -0,0 +1,116 @@
+use aoc_lib::point::Point;
+use std::collections::HashSet;
+use std::str::FromStr;
+
+use aoc_lib::input;
+use aoc_lib::point::Direction;
+
+#[derive(Debug)]
+struct Move {
+    dir: Direction,
+    cnt: usize,
+}
+
+impl FromStr for Move {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (dir, count) = s.split_once(" ").unwrap();
+        let dir = match dir {
+            "R" => Direction::PosX,
+            "L" => Direction::NegX,
+            "U" => Direction::PosY,
+            "D" => Direction::NegY,
+            _ => unreachable!(),
+        };
+
+        Ok(Move {
+            dir,
+            cnt: count.parse().unwrap(),
+        })
+    }
+}
+
+fn main() {
+    _ = input::apply("input-day-09.txt", |f| {
+        let moves = f
+            .lines()
+            .map(|l| l.parse::<Move>().unwrap())
+            .collect::<Vec<_>>();
+
+        aoc_lib::timed(|| {
+            aoc_lib::timed(|| println!("{}", part1(&moves)));
+            aoc_lib::timed(|| println!("{}", part2(&moves)));
+        });
+    });
+}
+
+// allow me to explain:
+// vector magic in this case allows this case to **not** follow the rules of the game
+// specifically, for 1 tail we know that it will only move in the same column as the head, thus we can conclude this:
+// suppose the head is at position (xh, yh) and the tail at (xt, yt) 
+// the head is moved by a unit vector in one direction that is always coordinal, thus the new head position either:
+// (xh + 1, yh) or (xh - 1, yh) or (xh, yh + 1) or (xh, yh - 1), or more generalized (xh + dx, yh + dy) where either dx or dy is 0
+//                                A
+//                               /|
+//                            C // B
+// using the above example: A is the new head position B is the old one, C is the initial tail position
+// we know CA = CB + BA, thus CB = CA - BA and CB is the movement we need to apply to the tail
+fn part1(moves: &Vec<Move>) -> usize {
+    let mut head_pos = *Point::origin();
+    let mut tail_pos = *Point::origin();
+    let mut visited: HashSet<Point> = HashSet::new();
+    visited.insert(tail_pos);
+
+    for mov in moves {
+        let unit = mov.dir.to_point();
+        // vector magic
+        for _ in 0..mov.cnt {
+            head_pos += unit;
+
+            if tail_pos.distance_squared(&head_pos) > 2 {
+                tail_pos += head_pos - tail_pos - unit;
+                visited.insert(tail_pos);
+            }
+        }
+    }
+
+    visited.len()
+}
+
+// sadly here I was forced to follow the rules bc aoc, man Im so sadge
+fn part2(moves: &Vec<Move>) -> usize {
+    let mut snaek = [*Point::origin(); 10];
+    let mut visited = HashSet::new();
+    visited.insert(*Point::origin());
+
+    for mov in moves {
+        let unit = mov.dir.to_point();
+        for _ in 0..mov.cnt {
+            snaek[0] += unit;
+
+            for i in 0..9 {
+                let head = snaek[i];
+                let tail = &mut snaek[i + 1];
+
+                if tail.distance_squared(&head) > 2 {
+                    let mut distance_vector = head - *tail;
+
+                    // sadly here we have to follow the rules :sadge:
+                    if distance_vector.x == 0 || distance_vector.y == 0 {
+                        distance_vector = distance_vector / 2;
+                    } else {
+                        distance_vector.x = distance_vector.x / distance_vector.x.abs();
+                        distance_vector.y = distance_vector.y / distance_vector.y.abs();
+                    }
+
+                    *tail += distance_vector;
+                }
+            }
+
+            visited.insert(snaek[9]);
+        }
+    }
+
+    visited.len()
+}
